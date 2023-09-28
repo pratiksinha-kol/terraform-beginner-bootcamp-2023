95f88e3db71b3b000363a1c0ee108bf1e546e5bf
pratiksinha-kol <pratik.proriters@gmail.com>
2023-09-28 07:10:24 +0000

    #27 Enabled S3 static hosting. Uploaded index and error html to s3 static hosting bucket via teraform.

--- modules/terrahouse_aws/main.tf
+++ modules/terrahouse_aws/main.tf
@@ -18,3 +18,40 @@ resource "aws_s3_bucket" "website_bucket" {
     UserUuid = var.user_uuid
   }
 }
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_website_configuration
+resource "aws_s3_bucket_website_configuration" "website_configuration" {
+  bucket = aws_s3_bucket.website_bucket.bucket
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+}
+
+#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_object
+
+resource "aws_s3_object" "index" {
+  bucket = aws_s3_bucket.website_bucket.bucket
+  key    = "index.html"
+  source = "${path.root}/public/index.html"
+
+  # The filemd5() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
+  # etag = "${md5(file("path/to/file"))}"
+  etag = filemd5(var.index_html_filepath)
+}
+
+resource "aws_s3_object" "error" {
+  bucket = aws_s3_bucket.website_bucket.bucket
+  key    = "error.html"
+  source = "${path.root}/public/error.html"
+
+  # The filemd5() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
+  # etag = "${md5(file("path/to/file"))}"
+  etag = filemd5(var.error_html_filepath)
+}
--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "bucket_name" {
   description = "S3 bucket for static website"
   value = module.terrahouse_aws.bucket_name
 }
+
+output "s3_website_endpoint" {
+  description = "S3 bucket website Endpoint"
+  value = module.terrahouse_aws.website_endpoint
+}
