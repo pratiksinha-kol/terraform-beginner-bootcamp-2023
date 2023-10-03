351a33528be2288f1f1feca831773576c9569bd5
Pratik Sinha <125117631+pratiksinha-kol@users.noreply.github.com>
2023-10-03 15:11:20 +0530

    41 terratowns provider (#43)
    * #41 Setting up the skeleton for creating a Custom Terraform Provider
    
    * #41 Implement terraform provider block in Terraform

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,14 @@
-output "bucket_name" {
-  description = "S3 bucket for static website"
-  value = module.terrahouse_aws.bucket_name
-}
+# output "bucket_name" {
+#   description = "S3 bucket for static website"
+#   value = module.terrahouse_aws.bucket_name
+# }
 
-output "s3_website_endpoint" {
-  description = "S3 bucket website Endpoint"
-  value = module.terrahouse_aws.website_endpoint
-}
+# output "s3_website_endpoint" {
+#   description = "S3 bucket website Endpoint"
+#   value = module.terrahouse_aws.website_endpoint
+# }
 
-output "cloudfront_url" {
-  description = "CloudFront Distribution Domain Name URL"
-  value = module.terrahouse_aws.cloudfront_url
-}
+# output "cloudfront_url" {
+#   description = "CloudFront Distribution Domain Name URL"
+#   value = module.terrahouse_aws.cloudfront_url
+# }
